0a5e5dec236437fa90b55bba14d1297e5801656a
Nexy Dev <keagonfrye@gmail.com>
2022-09-24 22:07:57 -0400

    Made Things Look Better And Added A Comment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
-﻿public class Program{
-    public static void Main(string[] args){
+﻿public class Program {
+    public static void Main (string[] args) {
         Console.Write("What is your name? ");
         string? name = Console.ReadLine();
         Console.WriteLine("Hi " + name);
+        //end of code//
         Console.WriteLine("\nPress Any Key To Close");
         Console.ReadKey();
     }
