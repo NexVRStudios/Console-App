66c850f72f4db5fdf1a3de63f6b6ab9268a1de02
Nexy Dev <keagonfrye@gmail.com>
2022-09-24 21:59:04 -0400

    Created Console App

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program{
+    public static void Main(string[] args){
+        Console.Write("What is your name? ");
+        string? name = Console.ReadLine();
+        Console.WriteLine("Hi " + name);
+        Console.WriteLine("\nPress Any Key To Close");
+        Console.ReadKey();
+    }
+}
