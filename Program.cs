b7286b72f39abcf48a4c198254a31415e2b731fb
Nexy Dev <keagonfrye@gmail.com>
2022-09-25 05:25:50 -0400

    Asks For How Your Day Was Now

--- Program.cs
+++ Program.cs
@@ -2,7 +2,20 @@
     public static void Main (string[] args) {
         Console.Write("What is your name? ");
         string? name = Console.ReadLine();
-        Console.WriteLine("Hi " + name);
+        Console.Write("Hi " + name + ", how is your day? ");
+        string? day = Console.ReadLine();
+        if (day == "good") {
+            Console.WriteLine("Nice, my day was good too.");
+        }
+        if (day == "Good") {
+            Console.WriteLine("Nice, my day was good too.");
+        }
+        if (day == "bad") {
+            Console.WriteLine("That sucks, hope it gets better.");
+        }
+        if (day == "Bad") {
+            Console.WriteLine("That sucks, hope it gets better.");
+        }
         //end of code//
         Console.WriteLine("\nPress Any Key To Close");
         Console.ReadKey();
